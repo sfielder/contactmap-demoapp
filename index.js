6f0e44240da52c3fc0666c4fe7fa7baeb31ca24b
Scott Persinger <scottpersinger@gmail.com>
2015-02-26 10:45:20 -0800

    Geolocate Contact addresses on a mpa

--- index.js
+++ index.js
@@ -1,11 +1,115 @@
+var Sequelize = require('sequelize');
+var https = require('https');
+var async = require('async');
+
+var dburl = process.env.DATABASE_URL || 'postgres://localhost/herokuconnect';
+var db = new Sequelize(dburl, {
+				dialectOptions: {
+					ssl: dburl.indexOf('localhost') == -1
+				},
+				logging: false
+			 });
+
+
+var Contact = db.define('Contact', {
+	id: Sequelize.INTEGER,
+	sfid: Sequelize.STRING,
+	email: Sequelize.STRING,
+	name: Sequelize.STRING,
+	mailingcity: Sequelize.STRING,
+	mailingcountry: Sequelize.STRING,
+	mailingpostalcode: Sequelize.STRING,
+	mailingstate: Sequelize.STRING,
+	mailingstreet: Sequelize.STRING
+}, {
+	timestamps: false,
+	freezeTableName: true,
+	schema: 'salesforce',
+	tableName: 'contact'
+	}
+);
+
+Geocode = db.define('geocode', {
+    id: Sequelize.INTEGER,
+    address: Sequelize.STRING,
+    lat: Sequelize.FLOAT,
+    lon: Sequelize.FLOAT
+});
+
+var GOOGLEMAPS = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyB0ymRb6jQAsgtoUnMO10-x6CjUe0PZK88&address=';
+
+function geocode(address, callback) {
+	Geocode.find({where: {address: address}}).then(function(geocode) {
+		if (geocode) {
+			callback(geocode);
+		} else {
+			https.get(GOOGLEMAPS + encodeURIComponent(address), function(res) {
+				if (res.statusCode < 300) {
+					var body = '';
+				    res.on('data', function(chunk) {
+		    			body += chunk;
+		  			});
+		  			res.on('end', function() {
+						body = JSON.parse(body);
+						if (body.results && body.results.length > 0) {
+							var lat = body.results[0].geometry.location.lat;
+							var lon = body.results[0].geometry.location.lng;
+							Geocode.create({address: address, lat: lat, lon: lon}).then(function(geocode) {
+								callback(geocode);
+							});
+						} else {
+							callback(null);
+						}
+					});
+				}
+			});
+		}
+	});
+}
+
+var pois = [];
+
+db.sync();
+
+function geocode_contact(contact, callback) {
+	if (contact.values.mailingstreet || contact.values.mailingcity || contact.values.mailingstate) {
+		var addr = contact.values.mailingstreet + "," + contact.values.mailingcity + "," + contact.values.mailingstate;
+		geocode(addr, function(geocode) {
+			if (geocode) {
+				var rec = {name: escape(contact.values.name), lat: geocode.lat, lon:geocode.lon};
+				callback(null, rec);
+			} else {
+				callback();
+			}
+		});
+	} else {
+		callback();
+	}
+}
+
+function load_pois(callback) {
+	Contact.findAll({limit:200}).then(function(rows) {
+		async.map(rows, geocode_contact, callback);
+	});
+}
+
+
+
+// EXPRESS
+
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res) {
+	load_pois(function(error, pois) {
+		console.log("POIS: ", pois);
+		res.render('index', {pois: pois.filter(function(val) { return val })});
+	});
 });
 
 app.listen(app.get('port'), function() {
